4548745b868df61ba11bfdf9aa5a912cd0c8f5d3
asudbury <sudburya@gmail.com>
2020-07-18 11:24:08 +0000

    Deploying to gh-pages from  @ ace97868590eee0f07fb073e0edc0a87c8b68571 🚀

---
+++ precache-manifest.b0428b9d987a949ef35dbfff6af01c52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d86c754448f1830dbda91e203c6a1c",
+    "url": "/react-open-weather/index.html"
+  },
+  {
+    "revision": "c8280f24c222b25aea7b",
+    "url": "/react-open-weather/static/css/main.3ce9b47a.chunk.css"
+  },
+  {
+    "revision": "b31e1e15fa6b548970c4",
+    "url": "/react-open-weather/static/js/2.97d45432.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/react-open-weather/static/js/2.97d45432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8280f24c222b25aea7b",
+    "url": "/react-open-weather/static/js/main.ca3cbc45.chunk.js"
+  },
+  {
+    "revision": "ed2797605449bc655244",
+    "url": "/react-open-weather/static/js/runtime-main.7b28ead7.js"
+  },
+  {
+    "revision": "1bdecd609daa2dc275784d9fca12874e",
+    "url": "/react-open-weather/static/media/background.1bdecd60.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.780ae6bb89bb4293ad4804f912f89e1f.js"
+  "/react-open-weather/precache-manifest.b0428b9d987a949ef35dbfff6af01c52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-open-weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
